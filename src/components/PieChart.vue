745d36442f701b493cfd95dfa807d8060c9b9f8f
Ruei <ruei.wang@104.com.tw>
2023-03-07 23:22:14 +0800

    [feat] pie chart component

---
+++ src/components/PieChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pie-chart" :style="cssVariables">
+    <div class="pie-chart-inside">
+      <div>
+        <span class="num">{{ percent }}</span
+        ><span class="unit">%</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  percent: {
+    type: Number,
+    default: 100,
+  },
+});
+
+const cssVariables = computed(() => {
+  return {
+    '--deg': `${(props.percent * 360) / 100}deg`,
+  };
+});
+</script>
+<style lang="scss">
+.pie-chart {
+  padding-top: 100%;
+  border-radius: 50%;
+  position: relative;
+  background: conic-gradient(skyblue var(--deg), #eee var(--deg));
+  &-inside {
+    position: absolute;
+    width: 80%;
+    padding-top: 80%;
+    border-radius: 50%;
+    top: 50%;
+    left: 50%;
+    background-color: #fff;
+    transform: translate(-50%, -50%);
+
+    div {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      font-size: 24px;
+      transform: translate(-50%, -50%);
+
+      .num {
+        font-size: 30px;
+      }
+    }
+  }
+}
+</style>
