3acf8be4982f044fc004e723d7ccf373561b6cb3
Ruei <ruei.wang@104.com.tw>
2023-03-08 23:30:48 +0800

    [feat] api related function

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div></div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import useApi from '@/composable/useApi.js';
+
+async function getGeocoding(cityName) {
+  const { result, execute } = useApi();
+  await execute({
+    url: '/geo/1.0/direct',
+    params: {
+      q: cityName,
+      limit: 1,
+    },
+  });
+
+  const [target] = result.value;
+  return {
+    lat: target.lat,
+    lon: target.lon,
+  };
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+async function getWetherForecast(lat, lon) {
+  const { result, execute } = useApi();
+  await execute({
+    url: 'data/2.5/forecast',
+    params: {
+      lat,
+      lon,
+      units: 'metric',
+    },
+  });
+
+  return result;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+console.log(getGeocoding, getWetherForecast);
+</script>
 <style lang="scss">
 @use 'normalize.css/normalize.css';
 </style>
---
+++ src/composable/useApi.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { ref } from 'vue';
+
+const instance = axios.create({
+  baseURL: 'http://api.openweathermap.org',
+});
+
+/**
+ *
+ * @param {https://axios-http.com/docs/req_config} requestConfig
+ */
+export default function useApi() {
+  const isLoading = ref(false);
+  const result = ref(null);
+  const error = ref(null);
+
+  const execute = async (requestConfig) => {
+    isLoading.value = true;
+    error.value = null;
+
+    const param = {
+      appid: '9a4a20244478df4c646c687aa6a4a800',
+    };
+
+    requestConfig.params = Object.assign(param, requestConfig.params);
+
+    try {
+      const response = await instance.request(requestConfig);
+      result.value = response.data;
+
+      return response;
+    } catch (e) {
+      error.value = e;
+      result.value = null;
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return {
+    isLoading,
+    result,
+    error,
+    execute,
+  };
+}
